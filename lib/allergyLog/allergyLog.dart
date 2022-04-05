896bb5f8aadd6ff2be7c67cda9de0266a07fac1c
Andreasgdp <andreasgdp@gmail.com>
2022-04-05 15:54:37 +0200

    Added main page content

--- lib/allergyLog/allergyLog.dart
+++ lib/allergyLog/allergyLog.dart
@@ -1,40 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:log_allergy/shared/shared.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 
 class AllergyLogScreen extends StatelessWidget {
   const AllergyLogScreen({Key? key}) : super(key: key);
 
-  // This widget is the root of your application. 
+  // This widget is the root of your application.
   // This widget will contain a counter with buttons to increment or decrement the counter.
   // This widget will contain a selector with 3 options [Easy, Medium, Hard]
   // This widget will contain another selector with 3 options [Easy, Medium, Hard]
   @override
   Widget build(BuildContext context) {
-    var _counter;
-    var _incrementCounter;
+    int _counter = 0;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Log Allergy'),
       ),
-      body: Center(
+      // Counter widget with buttons to increment or decrement the counter.
+      body: Container(
+        padding: const EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const FlutterLogo(
+              size: 50,
+            ),
             const Text(
-              "You have pushed the button this many times:",
+              'Amount of marks',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            NumberInputWithIncrementDecrement(
+              controller: TextEditingController(),
             ),
+            const Text(
+              'Itching difficulty',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const MyStatefulDropdown(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      bottomNavigationBar: const BottomNavBar(),
+    );
+  }
+}
+
+class MyStatefulDropdown extends StatefulWidget {
+  const MyStatefulDropdown({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulDropdown> createState() => _MyStatefulDropdownState();
+}
+
+class _MyStatefulDropdownState extends State<MyStatefulDropdown> {
+  String dropdownValue = 'None';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      alignment: Alignment.center,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
       ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['None', 'Little', 'Medium', 'Very']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,15 +1,19 @@
 import 'package:log_allergy/home/home.dart';
 import 'package:log_allergy/login/login.dart';
+import 'package:log_allergy/profile/profile.dart';
 
 class Routes {
   static String home = '/';
   static String login = '/login';
+  static String profile = '/profile';
 
   static String getHomeRoute() => home;
   static String getLoginRoute() => login;
+  static String getProfileRoute() => login;
 
   static var appRoutes = {
     home: (context) => const HomeScreen(),
     login: (context) => const LoginScreen(),
+    profile: (context) => const ProfileScreen(),
   };
 }
